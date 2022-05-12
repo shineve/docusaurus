182d5810298ca931379418b1b5b1122b3839a402
shineve <shineve95@gmail.com>
2022-05-13 01:40:58 +0800

    config and dependencies update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -54,7 +54,7 @@ const config = {
         logo: {
           alt: 'shineve blog Logo',
           src: 'img/logo.jpg',
-          href: '/blog'
+          href: '/'
         },
         items: [
           {
